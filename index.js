32d6c81f7742464bfac8d8e78a58b540d81f3a1e
LeoD3v <leoargusa1991@gmail.com>
2023-08-11 22:42:16 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    let highestValue = this.items.sort((a, b) => a - b);
+    return highestValue[highestValue.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    let highestValue = this.items.sort((a, b) => a - b);
 
-  sum() {}
+    return highestValue[0];
+  }
 
-  avg() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    }
+    let sum = 0;
+    this.items.map((e) => (sum += e));
+    return sum;
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    let sum = 0;
+    this.items.map((e) => (sum += e));
+    return sum / this.items.length;
+  }
 }
 
 module.exports = SortedList;
